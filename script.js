6e8b6f5224aa1e9c9ba0d0feece966cfc99eacc2
Deepak Kumar <142822780+x-deepak@users.noreply.github.com>
2025-02-18 21:30:01 +0530

    delete book feature and dialog skeleton added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const library = [];
 
+let container = document.querySelector(".container");
 
 function Book(title,author,pages,readStatus){
     this.title =title;
@@ -23,12 +24,12 @@ function addBookToLibrary(title,author,pages,readStatus) {
 }
 
 
-function cardGenerator(title,author,pages,readStatus){
+function cardGenerator(title,author,pages,readStatus,index){
     let card = document.createElement("div")
     card.classList.add("card")
     let html_str=`
     
-    <button class="card-close">X</button>
+    <button class="card-close" id="${index}">X</button>
     <table>
                 <tr>
                     <td id="title">Title</td>
@@ -55,8 +56,61 @@ function cardGenerator(title,author,pages,readStatus){
     return card;
 }
 
-let container = document.querySelector(".container");
 
-let cardElem = cardGenerator("Noob", "PK Rwling daf dafsd", "232", "read");
 
-container.appendChild(cardElem);
+
+
+
+function load_Library_In_Container(){
+    container.innerHTML = "";
+    library.forEach((obj, index) => {
+        let cardElem = cardGenerator(obj.title, obj.author, obj.pages, obj.readStatus, index);
+        container.appendChild(cardElem);
+    })
+}
+
+
+
+function remove_book_from_library(index){
+    // console.log(typeof index);
+    library.splice(index, 1);
+}
+
+
+function delete_book(event){
+    if (event.target.classList.contains("card-close")){
+        remove_book_from_library(event.target.id)
+        load_Library_In_Container();
+    }
+}
+
+
+addBookToLibrary("harry", "noob", "2323", true);
+load_Library_In_Container();
+
+container.addEventListener("click", delete_book)
+
+
+let add_book_btn = document.querySelector("#add-book-btn");
+
+
+let dialog = document.querySelector("dialog");
+
+add_book_btn.addEventListener("click", event => {
+    
+    let dialog = document.querySelector("dialog");
+    
+    dialog.showModal();
+
+});
+
+
+let submit_btn = document.querySelector("#submit-btn");
+
+submit_btn.addEventListener("click", event =>{
+    event.preventDefault();
+    let title_input = document.querySelector("#title");
+    let title = title_input.value;
+    console.log(title);
+    dialog.close();
+});
